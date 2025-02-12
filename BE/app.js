ccd1c3aabe2fc6a804ceb2fecd2bfa1c457291c6
Aniket Ghormare <aniketghormare62@gmail.com>
2025-02-12 15:50:28 +0530

    second

--- BE/app.js
+++ BE/app.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use("/auth",userRouter)
 app.use("/user",udharRouter)
 
-let PORT=process.env.PORT || 7000
+let PORT=process.env.PORT || 8000
 app.listen(PORT,async()=>{
     await connection
     console.log(`Server is running on port ${PORT}`)
